3280e181c0bcf92a03aafdf47661d16b69fbad6a
Maxim Kremenev <ezo@kremenev.com>
2013-09-19 20:24:33 +0700

    add reload action on monit service

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,10 @@ end
 
 package "monit"
 
-if platform?("ubuntu")
-  cookbook_file "/etc/default/monit" do
-    source "monit.default"
-    owner "root"
-    group "root"
-    mode 0644
-  end
-end
-
 service "monit" do
   action [:enable, :start]
   enabled true
-  supports [:start, :restart, :stop]
+  supports [:start, :restart, :stop, :reload]
 end
 
 directory node['monit']['config_d'] do
