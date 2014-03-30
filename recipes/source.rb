c2fd2ae448f8fd76471c6c0c1641eda920d8d2d3
Maxim Kremenev <ezo@kremenev.com>
2014-03-30 11:04:33 +0700

    fix

--- recipes/source.rb
+++ recipes/source.rb
@@ -13,7 +13,7 @@ bash 'install_monit_source' do
   cwd "#{Chef::Config[:file_cache_path]}"
   # action :nothing
   code <<-EOH
-  tar xvf #{file}.tar.gz && cd #{file}
+  tar xvf #{file}.tar.gz && cd monit-src-#{node[:monit][:source][:version]}
   ./configure --without-pam --prefix=#{node[:monit][:source][:install_dir]} && make && make install
   sed --in-place '116,121d;' monitrc
   cp -rf monitrc #{node[:monit][:config]} && chmod 0700 #{node[:monit][:config]}
